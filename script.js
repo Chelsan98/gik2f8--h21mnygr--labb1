a275959ee60c241dca1209dd158d7e69b55c0a8a
Chelsan98 <96055501+Chelsan98@users.noreply.github.com>
2022-12-09 12:33:03 +0100

    Allt klart

--- script.js
+++ script.js
@@ -27,21 +27,58 @@ function renderBookList(bookList) {
   //   element.addEventListener("mouseenter", ()=>{debugger;}) 
   // });
   
-  
+
   existingElement && root.removeChild(existingElement);
   bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', BookList(bookList));
   var bookHovers =document.querySelectorAll('.book-list__item');
   bookHovers.forEach(element => {
-    element.addEventListener("mouseenter",()=>{
+    element.addEventListener("mouseenter",()=>{ // Lägger en eventlistener på alla element som aktiveras när muspekaren hovrar över ett element.
      bookList.forEach(index => {
       if (element.textContent.includes(index.author +' - '+ index.title)){
-        /*skapa fönster här */
-        debugger;
+        createPanel(index);
       }
+
      })
     }) 
+    element.addEventListener("mouseout",()=> { // Ett litet problem här är att om man raderar det man har skrivit i sökfältet när rutan är uppe så stannar den kvar.
+     var test = document.querySelector('.hover')
+     if (test){
+      document.body.removeChild(test);
+     }
+    })
   });
 }
+  async function createPanel(index){
+  var link ='https://gik2f8-labs.herokuapp.com/books/'+ index.id.toString() // All info om böcker sparas i index så vi kan komma åt id med hjälp av det och lägga till det i länken
+  const result = await fetch(link)
+  const data = await result.json()
+  .then((data) => {afterWaiting(data)})
+  .catch((e) => e);
+}
 
+function afterWaiting(result){
+  
+  var html = document.createElement("div");
+  var image = document.createElement("img");
+  // Alla styles för bilden
+  image.src= result.coverImage
+  image.style.height ='80%';
+  image.style.alignSelf='center';
+  image.style.paddingLeft='1rem';
+  image.style.paddingRight='1rem';
 
-
+  html.innerText ="Författare: "+ '\n' +result.author + '\n' +'\n'+"Titel: " +'\n' +result.title +'\n' +'\n' + "Släpptes:"+'\n' +result.releaseDate +'\n' + '\n' + "Antal sidor:"+'\n'+ result.pages;
+  html.appendChild(image);
+  html.className ='hover'
+  document.body.appendChild(html);
+// Alla styles på rutan
+  const divStyle = document.querySelector(".hover");
+  divStyle.style.backgroundColor='white';
+  divStyle.style.color = 'grey';
+  divStyle.style.height ='20rem';
+  divStyle.style.width ='20rem';
+  divStyle.style.display='flex';
+  divStyle.style.marginLeft='40%';
+  divStyle.style.marginTop='1rem';
+  divStyle.style.paddingLeft='1rem';
+}
